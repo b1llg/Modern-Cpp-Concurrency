852a5d4f033d1bd56806a3dab17e92aa20b01e19
b1llg <guyb1294@gmail.com>
2024-01-04 12:07:18 -0500

    Started exercise 1p5

--- Section1_Thread_management_guide/1p4_LaunchAThread.cpp
+++ Section1_Thread_management_guide/1p4_LaunchAThread.cpp
@@ -3,16 +3,37 @@
 
 void foo()
 {
-    printf("Hello from foo\n");
+    std::cout << "Hello from foo - tid::" << std::this_thread::get_id() << std::endl;
 }
+
+class callable_class
+{
+
+public:
+
+    void operator()()
+    {
+            std::cout << "Hello from class with function call operator - tid::" << std::this_thread::get_id() << std::endl;
+    }
+};
 
 int main()
 {
 
     std::thread thread1(foo);
+
+    callable_class obj;
+    std::thread thread2(obj);
+
+    std::thread thread3([]
+    {
+            std::cout << "Hello from lambda- tid::" << std::this_thread::get_id() << std::endl;
+    });
+
     thread1.join();
+    thread2.join();
+    thread3.join();
 
-    printf("Hello from main\n");
-
+    std::cout << "Hello from main - tid::" << std::this_thread::get_id() << std::endl;
     return 0;
 }
---
+++ Section1_Thread_management_guide/1p5_ThreadExercise.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <thread>
+
+void functionC()
+{
+    std::cout << "Hello from function C - tid::" << std::this_thread::get_id() << std::endl;
+}
+
+void functionA()
+{
+    std::cout << "Hello from function A - tid::" << std::this_thread::get_id() << std::endl;
+
+    std::thread threadC(functionC);
+}
+
+void functionB()
+{
+    std::cout << "Hello from function B - tid::" << std::this_thread::get_id() << std::endl;
+}
+
+
+
+int main()
+{   
+    std::thread threadA(functionA);
+    std::thread threadB(functionB);
+
+        std::cout << "Hello from main - tid::" << std::this_thread::get_id() << std::endl; 
+
+    return 0;
+}
