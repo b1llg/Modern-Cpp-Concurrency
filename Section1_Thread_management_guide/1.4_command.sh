416fee06b35d47e141785eaf0193522c5f8cd0fd
b1llg <guyb1294@gmail.com>
2024-01-04 11:21:02 -0500

    Modified gitignore

--- Section1_Thread_management_guide/1.4_command.sh
+++ Section1_Thread_management_guide/1.4_command.sh
@@ -1,3 +1,4 @@
-rm 1.4_LaunchAThread
-g++ -std=c++20 1.4_LaunchAThread.cpp -o 1.4_LaunchAThread
-./1.4_LaunchAThread
+rm -rf bin
+mkdir bin
+g++ -std=c++20 1.4_LaunchAThread.cpp -o bin/1.4_LaunchAThread
+./bin/1.4_LaunchAThread
